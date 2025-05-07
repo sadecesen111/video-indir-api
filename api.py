a8b942e4336c273d94c2fa506d19b189d9c23561
sadecesen111 <141454124+sadecesen111@users.noreply.github.com>
2025-05-08 01:54:19 +0300

    Update api.py

--- api.py
+++ api.py
@@ -47,7 +47,3 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))  # Railway için önemli
     app.run(host='0.0.0.0', port=port, debug=True)
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))  # Railway için önemli
-    app.run(host='0.0.0.0', port=port, debug=True)
-

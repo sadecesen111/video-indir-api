9c9aaa0cd0703939fe9480b5356ccccdf27a7043
sadecesen111 <141454124+sadecesen111@users.noreply.github.com>
2025-05-08 01:57:16 +0300

    Update api.py

--- api.py
+++ api.py
@@ -46,4 +46,3 @@ if __name__ == '__main__':
     # Railway için PORT çevre değişkenini al
     port = int(os.environ.get("PORT", 8080))  # Railway için önemli
     app.run(host='0.0.0.0', port=port, debug=True)
-
